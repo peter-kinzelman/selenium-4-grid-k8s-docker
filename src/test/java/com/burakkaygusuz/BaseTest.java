0d58aa10e46fb51efbd72d5edaf93f5279d7c942
Burak Kaygusuz <burak.kaygusuz@inomera.com>
2021-01-30 11:21:46 +0300

    Logging structure is changed

--- src/test/java/com/burakkaygusuz/BaseTest.java
+++ src/test/java/com/burakkaygusuz/BaseTest.java
@@ -4,13 +4,11 @@ import com.burakkaygusuz.config.DriverFactory;
 import com.burakkaygusuz.config.DriverType;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.apache.logging.log4j.core.LoggerContext;
 import org.junit.jupiter.api.*;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.io.File;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -20,15 +18,10 @@ public class BaseTest {
     public static final List<DriverFactory> webDriverThreadPool = Collections.synchronizedList(new ArrayList<>());
     public static ThreadLocal<DriverFactory> driverFactoryThread;
 
-    public static final Logger log = LogManager.getLogger(BaseTest.class);
-    private static final LoggerContext context = (LoggerContext) LogManager.getContext(false);
+    private static final Logger log = LogManager.getLogger(BaseTest.class.getName());
 
     public RemoteWebDriver driver;
     public WebDriverWait wait;
-
-    public BaseTest() {
-        context.setConfigLocation(new File("src/test/resources/log4j2.properties").toURI());
-    }
 
     @BeforeAll
     public static void setUp() {
