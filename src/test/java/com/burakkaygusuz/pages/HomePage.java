2f0c04280af981e3e4f54b2c970c7e8b3ab84d98
Burak Kaygusuz <burak.kaygusuz@inomera.com>
2021-01-30 11:28:05 +0300

    Some logs added for operations on the home page

--- src/test/java/com/burakkaygusuz/pages/HomePage.java
+++ src/test/java/com/burakkaygusuz/pages/HomePage.java
@@ -1,13 +1,17 @@
 package com.burakkaygusuz.pages;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.Select;
 
+public class HomePage {
 
     protected RemoteWebDriver driver;
+    private static final Logger log = LogManager.getLogger(HomePage.class.getName());
     private static final String BASE_URL = "https://wikipedia.org";
 
     @FindBy(id = "searchInput")
@@ -31,17 +35,20 @@ import org.openqa.selenium.support.ui.Select;
     public HomePage selectLanguage(String language) {
         Select dropdownList = new Select(selectLanguage);
         dropdownList.selectByVisibleText(language);
+        log.info(String.format("The %s language is selected", language));
         return this;
     }
 
     public HomePage clearAndType(String input) {
         searchInput.clear();
         searchInput.sendKeys(input);
+        log.info(String.format("'%s' has been written in %s", input, searchInput));
         return this;
     }
 
     public HomePage submitButton() {
         searchButton.click();
+        log.info(String.format("%s button has been submitted", searchButton));
         return this;
     }
 }
