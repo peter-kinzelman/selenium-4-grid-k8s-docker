033dd8f41d3b52cf6c695c9fecad9b205d59902e
Burak Kaygusuz <kaygusuzburak@gmail.com>
2021-04-03 11:17:42 +0300

    Refactor the parameterized test

--- src/test/java/com/burakkaygusuz/tests/SeleniumPageTests.java
+++ src/test/java/com/burakkaygusuz/tests/SeleniumPageTests.java
@@ -1,46 +1,53 @@
 package com.burakkaygusuz.tests;
 
-import com.burakkaygusuz.BaseTest;
+import com.burakkaygusuz.config.DriverBase;
 import com.burakkaygusuz.enums.Browsers;
 import com.burakkaygusuz.pages.HomePage;
 import com.burakkaygusuz.pages.SeleniumPage;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.parallel.Execution;
 import org.junit.jupiter.api.parallel.ExecutionMode;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.EnumSource;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 import static com.burakkaygusuz.pages.HomePage.getHomePage;
 import static com.burakkaygusuz.pages.SeleniumPage.getSeleniumPage;
-import static org.junit.jupiter.api.Assertions.*;
 
 @Execution(ExecutionMode.CONCURRENT)
 @DisplayName("Selenium Wikipedia Page Tests")
-public class SeleniumPageTests extends BaseTest {
+public class SeleniumPageTests extends DriverBase {
 
     @ParameterizedTest(name = "#{index} - Run with => {0}")
-    @EnumSource(value = Browsers.class, names = {"CHROME", "FIREFOX", "EDGE"})
-    public void getSeleniumWikiPage(Browsers browser) {
+    @EnumSource(value = Browsers.class, names = {"CHROME", "FIREFOX"})
+    public void navigateToSeleniumWikiPage(Browsers browser) {
 
-        driver = getWebDriver(browser);
-        wait = getDriverWait(driver);
+        RemoteWebDriver driver = DriverBase.getDriver(browser);
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10), Duration.ofMillis(1000));
 
         final HomePage homePage = getHomePage(driver);
         final SeleniumPage wikiPage = getSeleniumPage(driver);
 
         driver.navigate().to("https://wikipedia.org");
-
-        homePage.selectLanguage("English")
-                .clearAndType("Selenium (software)")
-                .submitButton();
+        homePage.goToSeleniumWikiPage();
 
         wait.until(ExpectedConditions.urlContains("https://en.wikipedia.org/wiki/Selenium_(software)"));
 
-        assertAll("Selenium wiki page should open as successfully",
-                () -> assertEquals("Selenium (software) - Wikipedia", driver.getTitle()),
-                () -> assertTrue(wikiPage.getDefinitionText().startsWith("This article is about the software testing framework")));
+        Assertions.assertThat(driver.getTitle())
+                .withFailMessage(String.format("Expected page title is 'Selenium (software) - Wikipedia' but was '%s'", driver.getTitle()))
+                .isEqualTo("Selenium (software) - Wikipedia");
+
         Assertions.assertThat(wikiPage.getShortDescription())
+                .withFailMessage("Short description is different")
+                .startsWith("This article is about the software testing framework");
+    }
+
     @Test
     @DisplayName("Verify The Github Url Is Correct")
     public void verifyTheGithubPageUrlIsCorrect() {
